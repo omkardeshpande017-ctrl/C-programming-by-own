aa3f0076cc69cb5caed0d31c1a1c8d5dd242507c
omkardeshpande017-ctrl <omkardeshpande017@gmail.com>
2025-11-07 22:11:52 +0530

    Marks
    to find the marks and grade of students

---
+++ marks.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+int main(){
+
+    int marks;
+    printf("Enter your marks: ");
+    scanf("%d", &marks);
+
+    if(marks >=90 && marks <=100){
+        printf("Grade: A+\n");
+    }
+    else if(marks >=80 && marks <90){
+        printf("Grade: A\n");
+    }
+    else if (marks >=70 && marks <80){
+        printf("Grade: B+\n");
+    }
+    else if 
+    
+    (marks >=60 && marks <70){
+        printf("Grade: B\n");
+    }
+    else if (marks >=50 && marks <60){
+        printf("Grade: C+\n");
+    }
+    else if (marks >=40 && marks <50){
+        printf("Grade: C\n");
+    }
+    else if (marks >=35 && marks <40){
+        printf("Grade: D\n");
+    }
+    else if (marks >=0 && marks <35){
+        printf("grade is fail: F\n");
+    }
+    
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
